241eba843bd34b59c2b7e111151a4dc756657b3f
Eduardo Eiji <edu_eiji@hotmail.com>
2020-05-20 11:38:40 -0300

    login_goBArber

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
-export default function SignUp() {
-  return <div />;
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input palceholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Ja tenho login</Link>
+      </form>
+    </>
+  );
 }
